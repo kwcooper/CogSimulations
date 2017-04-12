f86f30966bd32671c30bb378caf1456532ad183e
Keiland <kc42@149-160-205-202.dhcp-bl.indiana.edu>
2017-04-12 02:57:09 -0400

    ud

--- neuralNets/nnRead.py
+++ neuralNets/nnRead.py
@@ -1,131 +1,125 @@
-from numpy import exp, array, random, dot
+import random
+from numpy import random, exp, array, dot
 import matplotlib.pyplot as plt
 
 class Layer():
     def __init__(self, numNeurons, numInputs):
-        #creates a random matix of size inputs by neurons
-        self.synaptic_weights = 2 * random.random((numInputs, numNeurons)) - 1
+        #creates a random matix of size (inputs by neurons)
+        self.weights = 2 * random.random((numInputs, numNeurons)) - 1
 
-
-# normalise them between 0 and 1.
 def sigmoid(x):
     return 1 / (1 + exp(-x))
 
 # The derivative of the Sigmoid function to calculate error
 def derivative(x):
     return x * (1 - x)
-    
-class NeuralNetwork():
-    #can this be molded into n layers?
-    def __init__(self, layer1, layer2):
-        self.layer1 = layer1
-        self.layer2 = layer2
 
+#the neural network
+class Net():
+    #can this be modded into n layers?
+    def __init__(self):
+        self.layer1 = Layer(4, 2)
+        self.layer2 = Layer(1, 4)
+
+        self.startWeights = []
+        self.errors = []
 
     # The network thinks.
     def forward(self, inputs):
-        outLayer1 = sigmoid(dot(inputs, self.layer1.synaptic_weights))
-        outLayer2 = sigmoid(dot(outLayer1, self.layer2.synaptic_weights))
+        outLayer1 = sigmoid(dot(inputs, self.layer1.weights))
+        outLayer2 = sigmoid(dot(outLayer1, self.layer2.weights))
         return outLayer1, outLayer2
     
-    #
+    
     def train(self, trainingInputs, targets, iterations):
         for iteration in range(iterations):
-            # Pass the training set through our neural network
             outputLayer1, outputLayer2 = self.forward(trainingInputs)
 
-            # Calculate the error for layer 2 (The difference between the desired output
-            # and the predicted output).
             errorLayer2 = targets - outputLayer2
+            self.errors.append(errorLayer2)
+            print(errorLayer2)
             deltaLayer2 = errorLayer2 * derivative(outputLayer2)
-
-            # Calculate the error for layer 1 (By looking at the weights in layer 1,
-            # we can determine by how much layer 1 contributed to the error in layer 2).
-            errorLayer1 = deltaLayer2.dot(self.layer2.synaptic_weights.T)
+            
+            errorLayer1 = deltaLayer2.dot(self.layer2.weights.T)
             deltaLayer1 = errorLayer1 * derivative(outputLayer1)
 
-            # Calculate how much to adjust the weights by
-            layer1_adjustment = trainingInputs.T.dot(deltaLayer1)
-            layer2_adjustment = outputLayer1.T.dot(deltaLayer2)
+            adjustmentLayer1 = trainingInputs.T.dot(deltaLayer1)
+            adjustmentLayer2 = outputLayer1.T.dot(deltaLayer2)
 
-            # Adjust the weights.
-            self.layer1.synaptic_weights += layer1_adjustment
-            self.layer2.synaptic_weights += layer2_adjustment
+            self.layer1.weights += adjustmentLayer1
+            self.layer2.weights += adjustmentLayer2
 
 
     # The neural network prints its weights
-    def print_weights(self):
-        print("    Layer 1 (4 neurons, each with 3 inputs): ")
-        print(self.layer1.synaptic_weights)
-##        plt.matshow(self.layer1.synaptic_weights, cmap="Greys")
-##        plt.show()
-        print( "    Layer 2 (1 neuron, with 4 inputs):")
-        print(self.layer2.synaptic_weights)
+    def showWeights(self, strt):
+        print("First Layer: ")
+        print(self.layer1.weights)
+        print( "Second Layer: ")
+        print(self.layer2.weights.T)
+        if strt == False:
+            pss = '(Start)'
+        elif strt == True:
+            pss = '(End)'
+            # if you prefur imshow gradients...
+        plt.subplot(2,1,1)
+        plt.imshow(self.layer1.weights, cmap="Greys", interpolation='nearest')
+        plt.axis('off')
+        plt.title('First Layer Weights ' + pss)
+        
+        plt.subplot(2,1,2)
+        plt.imshow(self.layer2.weights, cmap="Greys", interpolation='nearest')
+        plt.axis('off')
+        plt.title('Second Layer Weights')
+        plt.show()
 
+    def showError(self):
+        plt.plot(self.errors)
+        plt.title('Training Error')
+        plt.show()
 
+#run the network
+net = Net()
 
-##
-###Seed the random number generator
-##random.seed(1)
-##
-### Create layer 1 (4 neurons, each with 3 inputs)
-##layer1 = Layer(4, 3)
-##
-### Create layer 2 (a single neuron with 4 inputs)
-##layer2 = Layer(1, 4)
-##
-### Combine the layers to create a neural network
-##neural_network = NeuralNetwork(layer1, layer2)
-##
-##print("Stage 1) Random starting synaptic weights: ")
-##neural_network.print_weights()
-##
-### The training set. We have 7 examples, each consisting of 3 input values
-### and 1 output value.
-##trainingInputs = array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
-##targets = array([[0, 1, 1, 1, 1, 0, 0]]).T
-##
-### Train the neural network using the training set.
-### Do it 60,000 times and make small adjustments each time.
-##neural_network.train(trainingInputs, targets, 60000)
-##
-##print( "Stage 2) New synaptic weights after training: ")
-##neural_network.print_weights()
-##
-### Test the neural network with a new situation.
-##print( "Stage 3) Considering a new situation [1, 1, 0] -> ?: ")
-##hidden_state, output = neural_network.forward(array([1, 1, 0]))
-##print( output)
-
-
-
-# Create layer 1 (4 neurons, each with 3 inputs)
-layer1 = Layer(4, 2)
-
-# Create layer 2 (a single neuron with 4 inputs)
-layer2 = Layer(1, 4)
-
-# Combine the layers to create a neural network
-neural_network = NeuralNetwork(layer1, layer2)
-
-print("Stage 1) Random starting synaptic weights: ")
-neural_network.print_weights()
-
-# The training set. We have 7 examples, each consisting of 3 input values
-# and 1 output value.
+print("RANDOM WEIGHTS: ")
+net.showWeights(False)
+print(' ')
 trainingInputs = array([[0, 0], [0, 1], [1, 0], [1, 1]])
 targets = array([[1, 0, 0, 1]]).T
 
-# Train the neural network using the training set.
-# Do it 60,000 times and make small adjustments each time.
-neural_network.train(trainingInputs, targets, 60000)
+net.train(trainingInputs, targets, 60000)
 
-print( "Stage 2) New synaptic weights after training: ")
-neural_network.print_weights()
+print("NEW WEIGHTS:")
+net.showWeights(True)
+print(' ')
 
-# Test the neural network with a new situation.
-print( "Stage 3) Considering a new situation [1, 1] -> ?: ")
-hidden_state, output = neural_network.forward(array([1, 1]))
-print( output)
+net.showError()
+
+print( "Test the network")
+hidden, output = net.forward(array([1, 1]))
+print('We expect 1, we got: ', output)
+#print(hidden)
 
 
+
+
+
+
+
+
+##def plotPerceptron(size,runs):
+##    points = generatePoints(size, getLinearTarget)
+##    plotPoints(points)
+##    p = Perceptron()
+##    for run in range(runs):
+##        guesses = []
+##        for point in points:
+##            x,y,t = point
+##            g = p.train(x,y,t)
+##            guesses.append((x,y,g))
+##        plotPoints(guesses)
+##    plt.show()
+##
+##def plotNeuralNet(size, runs):
+##    points = generatePoints(size, getLinearTarget)
+##    plotPoints()
+##    
