d29d203bf7a2b86596b23d6f83e0f9f99c3bbcb9
Keiland <officialkwade@gmail.com>
2017-06-02 00:34:51 -0400

    ud

--- izhikevichModel/network/izhNetKv3.py
+++ izhikevichModel/network/izhNetKv3.py
@@ -4,6 +4,7 @@ import networkx as nx
 
 #needs to be optimized for fewer matricies
 #needs random starting point
+#[ROW][Collumn]
 
 numNeurons = 50
 steps = 1
@@ -13,6 +14,9 @@ maxTme = int(np.ceil(steps/.025))
 #probably unessary...
 dtMat = .025 *np.ones((numNeurons, 1), dtype=np.float32)
 
+tMinusZs = np.zeros((numNeurons, maxTme-1), dtype=np.float32)
+
+
 #do I really need individual matrices for these?
 ##aMat = .02*np.ones((numNeurons, 1), dtype=np.float32)
 ##bMat = .2*np.ones((numNeurons, 1), dtype=np.float32)
@@ -20,9 +24,14 @@ dtMat = .025 *np.ones((numNeurons, 1), dtype=np.float32)
 ##dMat = 2. * np.ones((numNeurons, 1), dtype=np.float32)
 
 #vMat = -65. * np.ones((numNeurons, 1), dtype=np.float32)
-vMat = -65 * np.random.randint(2, size=(numNeurons, maxTme))
+#vMat = -65 * np.random.randint(2, size=(numNeurons, maxTme))
+
+#concat random start with the zero matrix at axis 1
+vMat = np.append(-65 * np.random.randint(2, size=(numNeurons, 1)), tMinusZs, 1)
 print('vMat:\n', vMat)
 uMat = 0. * np.random.randint(2, size=(numNeurons, maxTme))
+#uMat = np.append(0.0 * np.random.randint(2, size=(numNeurons, 1)), tMinusZs, 1)
+
 
 dtN = np.ones((numNeurons, maxTme), dtype=np.float32)
 vN = np.ones((numNeurons, maxTme), dtype=np.float32) 
@@ -44,13 +53,13 @@ plt.title('Spikes')
 plt.show()
 
 
-def getI(vMat, connect, i):
+def getI(vMat, connect, i, step):
     #iterates through fired, if connection, then increments input I
     #Should make the 'bias' a parameter
     #this could be where hebbian learning etc. could be implimented
     I = 0
     for j in range(0, numNeurons):
-        if vMat[i] > 25 and connect[i][j] == 1:
+        if vMat[i][step] > 25 and connect[i][j] == 1:
             I += 1
     #print('Found ', I)
     return I + 2
@@ -66,8 +75,8 @@ while step < cnt:
     for i in range(0, numNeurons):
         I = getI(vMat, connect, i)
 
-        u = uMat[i]
-        v = vMat[i]
+        u = uMat[i][step]
+        v = vMat[i][step]
 ##        a = aMat[i]
 ##        b = bMat[i]
 ##        c = cMat[i]
@@ -90,12 +99,12 @@ while step < cnt:
         v = v + dt*dv
         u = u + dt*du
 
-        uN[i] = u
-        vN[i] = v
+        uN[i][step] = u
+        vN[i][step] = v
         #print('new vMat:\n', vMat)
 
-    uMat = uN
-    vMat = vN
+##    uMat = uN
+##    vMat = vN
     #print(vMat)
 
     t += dt
@@ -103,9 +112,9 @@ while step < cnt:
 
     
 plt.matshow(spikes, cmap=plt.cm.gray)
-plt.title('Spikes')
+plt.title('Post-Spikes')
 plt.show()
 
 plt.matshow(vMat, cmap=plt.cm.gray)
-plt.title('Voltage')
+plt.title('Post-Voltage')
 plt.show()
