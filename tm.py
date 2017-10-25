1ae38118dac6a9339459eaf7c0d761f6356e62e2
Keiland <officialkwade@gmail.com>
2017-10-25 15:15:07 -0400

    Init

---
+++ tm.py
@@ -0,0 +1,27 @@
+alphabet = {}
+
+#instruction set = (state1, if this, then this, state2)
+instructions = {}
+
+class Tape:
+    def __init__(self):
+        self.tape = []
+#print(instructions)
+
+class Head:
+    def __init__(self):
+
+
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
+    def erase(self):
+        pass
+
+    
+        
+
+
