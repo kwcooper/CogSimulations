31007616decf499d117b3fd40e4ace56a40f63be
Keiland <officialkwade@gmail.com>
2017-11-13 12:37:04 -0500

    Ud

--- neuralNets/scaleNN.py
+++ neuralNets/scaleNN.py
@@ -1,2 +1,8 @@
 #neural net that can scale the number of nodes, edges, and layers
 
+# probably the best bet is to add each of the matricies to a data struct?
+# but how will we handle for the connectome?
+
+# what would the connectome of a multilayer net look like?
+
+
